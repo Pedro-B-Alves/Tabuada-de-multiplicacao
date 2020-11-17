5b411862406b45fab73b324bb7611f3c6a2541f1
Pedro Alves <pedro22carro@gmail.com>
2020-11-17 16:40:35 -0300

    Cálculo finalizado

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Tabuada_de_multiplicacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o número que você quer ver a tabuada");
+            int numero = int.Parse(Console.ReadLine());
+            Console.WriteLine("-------------------------------------------");
+            for (var contador = 0; contador < 11; contador++)
+            {
+                Console.WriteLine(numero+" X "+contador+" = "+numero*contador);
+            }
+        }
+    }
+}
